ed0a198c1f165ddaed90ead24c70f2d48dc3f89d
kirill-the-creator <13zabl@mail.ru>
2025-05-02 19:57:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        const targetId = this.getAttribute('href');
+        const target = document.querySelector(targetId);
+        if (target) {
+            e.preventDefault();
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
